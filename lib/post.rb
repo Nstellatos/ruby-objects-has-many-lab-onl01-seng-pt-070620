b161668f77aecc9fd9e991741133b0f86e1b9b11
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-03 00:11:40 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,11 @@
 class Post
-  attr_accessor :author, :title 
+  attr_accessor :author, :title
 
   def initialize(title)
     @title = title
   end
 
   def author_name
-    if author 
-      self.author.name 
-    else
-      nil
-    end
+    self.author ? self.author.name : nil
   end
-
 end 
