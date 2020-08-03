c6b12664a3f861bcfa50e8f069e6b51fed6dbdbf
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-03 00:11:24 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
-class Author 
+class Author
   attr_accessor :name
+
   @@post_count = 0
 
   def initialize(name)
@@ -7,13 +8,17 @@ class Author
     @posts = []
   end
 
-   def posts	
-    Post.all.select {|post| post.author == self}	
+  def posts
+    @posts
   end
 
-  def add_post(post)
-    @posts << post
-    post.author = self
+  def self.post_count
+    @@post_count
+  end
+
+  def add_post(post_title)
+    @posts << post_title
+    post_title.author = self
     @@post_count += 1
   end
 
@@ -23,9 +28,4 @@ class Author
     post.author = self
     @@post_count += 1
   end
-
-  def self.post_count
-    @@post_count
-  end
-
 end 
